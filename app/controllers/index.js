e117862393457a8225e168feb16189c80b054976
Elijah Windsor <ewindsor@gmail.com>
2013-02-05 17:15:10 -0800

    showing memory issue

---
+++ app/controllers/index.js
@@ -0,0 +1,27 @@
+$.index.open(); 
+
+$.view = Alloy.createController('view');
+
+$.index.add($.view.getView());
+
+
+function removeViewAndClose() {
+	// this attempts to remove items from the window before closing it
+	$.index.remove($.view.getView());
+
+	$.index.close(); 
+}
+
+function justClose() {
+	$.index.close();
+}
+
+
+//Run this in Instruments, and filter for TiUIView. 
+// TODO: choose which one to Run:
+
+//justClose();  // this will release the TiUIView
+
+// or
+
+//removeViewAndClose();  // TiUIView will stay in memory here, and is forever forgotten....
